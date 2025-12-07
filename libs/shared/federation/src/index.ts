fb0b8bd535d6ff7f43c0111ffb11cbe2debd3d10
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-12-07 15:39:02 +0100

    build: update federation api, deep linking support

--- libs/shared/federation/src/index.ts
+++ libs/shared/federation/src/index.ts
@@ -1,12 +1,16 @@
 
 export * from './lib/custom-element/custom-element.provider';
+export * from './lib/mfe-discovery/mfe.config';
+export * from './lib/mfe-discovery/mfe.integration';
 export * from './lib/mfe-integration/default.provider';
 export * from './lib/multi-version-loader/multi-version.config';
 export * from './lib/multi-version-loader/multi-version.model';
 export * from './lib/multi-version-loader/multi-version.routing';
 export * from './lib/multi-version-loader/multi-version.wrapper';
+export * from './lib/router/deep-linking';
 export * from './lib/router/location-strategy';
 export * from './lib/router/router.inject';
 export * from './lib/router/route-segment-matcher';
 export * from './lib/router/router.provider';
+export * from './lib/router/url-handling';
 export * from './lib/zone/zone.provider';
